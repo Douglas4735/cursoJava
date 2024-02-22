0fd1725e59899eb34adc5afd76b5d41cf0ddc983
douglas4735 <douglas4735@gmail.com>
2024-02-21 20:37:19 -0400

    novas alterações sendo enviadas

--- cursoJava/src/cursoJava/executavel/PrimeiraClasseJava.java
+++ cursoJava/src/cursoJava/executavel/PrimeiraClasseJava.java
@@ -23,24 +23,31 @@ public class PrimeiraClasseJava {
 		aluno1.setNomePai("Davi");
 		aluno1.setDataMatricula("10/11/22");
 		aluno1.setSerieMatriculado("5");
+		aluno1.setNota1(90);
+		aluno1.setNota2(80);
+		aluno1.setNota3(80);
+		aluno1.setNota4(70);
 		
 		System.out.println("Nome aluno é: " + aluno1.getNome());
 		System.out.println("Idade aluno é: " + aluno1.getIdade());
-		System.out.println("Nome aluno é: " + aluno1.getDataNascimento());
-		System.out.println("Nome aluno é: " + aluno1.getRegistroGerel());
-		System.out.println("Nome aluno é: " + aluno1.getNumeroCpf());
-		System.out.println("Nome aluno é: " + aluno1.getNomeMae());
-		System.out.println("Nome aluno é: " + aluno1.getNomePai());
-		System.out.println("Nome aluno é: " + aluno1.getDataMatricula());
-		System.out.println("Nome aluno é: " + aluno1.getSerieMatriculado());
+		System.out.println("Data de nascimento do aluno é: " + aluno1.getDataNascimento());
+		System.out.println("RG do aluno é: " + aluno1.getRegistroGerel());
+		System.out.println("CPF do aluno é: " + aluno1.getNumeroCpf());
+		System.out.println("Nome da mãe aluno é: " + aluno1.getNomeMae());
+		System.out.println("Nome do pai do aluno é: " + aluno1.getNomePai());
+		System.out.println("Data da matricula do aluno é: " + aluno1.getDataMatricula());
+		System.out.println("Serie do aluno é: " + aluno1.getSerieMatriculado());
+		System.out.println("Média do aluno é : " + aluno1.getMediaNota());
 		
 		System.out.println("---------------------------------------------");
+		/*
+		Aluno aluno2 = new Aluno();
+		aluno2.setNome("fernanda");
+		aluno2.setIdade(25);
 		
-		/*Aluno aluno2 = new Aluno();
-		aluno2.nome = "fernanda";
 		
-		
-		System.out.println("Aluno: " + aluno2.nome);
+		System.out.println("Nome Aluno2: " + aluno2.getNome());
+		System.out.println("Idade Aluno2 é : " + aluno2.getIdade());
 		
 		System.out.println("---------------------");
 		
